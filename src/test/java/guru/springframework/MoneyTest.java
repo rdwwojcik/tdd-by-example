0a4c24a11abab426efdf4197bfef10df263048d4
Radek <rdwwojcik@gmail.com>
2022-07-13 10:45:44 +0200

    S3.24-TDD-Making objects

---
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -0,0 +1,38 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+public class MoneyTest {
+
+    @Test
+    void testMultiplicationDollar(){
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    void testEqualityDollar() {
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(8));
+    }
+
+    @Test
+    void testMultiplicationFranc(){
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
+    void testEqualityFranc() {
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(8));
+    }
+}
